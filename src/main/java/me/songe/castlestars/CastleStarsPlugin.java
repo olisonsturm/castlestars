29405d8325ccf41543ed5658962fb452a0739fe6
BuildTools <unconfigured@null.spigotmc.org>
2020-03-30 22:17:43 +0200

    [+] CastleStars Plugin Main 2

---
+++ src/main/java/me/songe/castlestars/CastleStarsPlugin.java
@@ -0,0 +1,17 @@
+package me.songe.castlestars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CastleStarsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
